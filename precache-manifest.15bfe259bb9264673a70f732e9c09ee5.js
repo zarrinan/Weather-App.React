348d7f270f6814f8238ab8d5b94c55b91bea8f53
Zarrina <zarrina_@hotmail.com>
2019-05-11 20:46:43 -0400

    Updates

---
+++ precache-manifest.15bfe259bb9264673a70f732e9c09ee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10e952cf2ac8fe11aa2bf5fea13a54b",
+    "url": "/Weather-App.React/index.html"
+  },
+  {
+    "revision": "efa0dd6c8699b83bfb91",
+    "url": "/Weather-App.React/static/css/main.cf08999e.chunk.css"
+  },
+  {
+    "revision": "e73db899b016111becfa",
+    "url": "/Weather-App.React/static/js/2.60531241.chunk.js"
+  },
+  {
+    "revision": "efa0dd6c8699b83bfb91",
+    "url": "/Weather-App.React/static/js/main.07109114.chunk.js"
+  },
+  {
+    "revision": "7adb9ff8531092c19cfb",
+    "url": "/Weather-App.React/static/js/runtime~main.2d403bd4.js"
+  }
+]);
